5adc759e86cf8a9e1f996d2b5c5e6e6fb4cd2ce4
unknown <anthony560@hotmail.com>
2020-12-08 18:15:13 -0400

    webpack config

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/components/app.css'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hasta ese dia me voy a Quedar</h1>
+        </div>
+    );
+}
+
+export default App;
