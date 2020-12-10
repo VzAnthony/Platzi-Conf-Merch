d6a677531424e56cbf225e9454c6726317725ab8
unknown <anthony560@hotmail.com>
2020-12-10 14:40:47 -0400

    React Router DOM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../styles/components/app.css'
+import React from 'react';
+
+import '../styles/components/app.css';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hasta ese dia me voy a Quedar</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: miniCssExtractPlugin.loader,
-                    },
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new miniCssExtractPlugin({
-            filename: 'assets/[name].css'
-        })
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000,
-    }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new miniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+  },
+};
