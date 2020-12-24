3555464601bfb09fcffba2c7b7e5f41bab70f665
Anthony Moreno <anthony560@hotmail.com>
2020-12-24 13:29:36 -0400

    react helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css'
@@ -17,30 +18,35 @@ const Checkout = () => {
 
 
     return (
-        <div className="Checkout">
-            <div className="Checkput-content">
-                {cart.length > 0 ? <h3>Lista de Pedidos: </h3> : <h3>Sin Pedidos...</h3>}
-                {cart.map((item, index) => (
-                    <div className="Checkout-item" key={`${item.id} + ${index}`}>
-                        <div className="Checkout-element">
-                            <h4>{item.title}</h4>
-                            <span>${item.price}</span>
+        <>
+            <Helmet>
+                <title>Lista de Pedidos - Platzi Conf Merch</title>
+            </Helmet>
+            <div className="Checkout">
+                <div className="Checkput-content">
+                    {cart.length > 0 ? <h3>Lista de Pedidos: </h3> : <h3>Sin Pedidos...</h3>}
+                    {cart.map((item, index) => (
+                        <div className="Checkout-item" key={`${item.id} + ${index}`}>
+                            <div className="Checkout-element">
+                                <h4>{item.title}</h4>
+                                <span>${item.price}</span>
+                            </div>
+                            <button type="button" onClick={() => handleRemoveFromCart(item.id)}>
+                                <i className="fas fa-trash-alt" />
+                            </button>
                         </div>
-                        <button type="button" onClick={() => handleRemoveFromCart(item.id)}>
-                            <i className="fas fa-trash-alt" />
-                        </button>
-                    </div>
-                ))}
-            </div>
-            {cart.length > 0 && (
-                <div className="Checkout-sidebar">
-                    <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-                    <Link to="/checkout/information">
-                        <button type="button">Continuar Pedido</button>
-                    </Link>
+                    ))}
                 </div>
-            )}
-        </div>
+                {cart.length > 0 && (
+                    <div className="Checkout-sidebar">
+                        <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+                        <Link to="/checkout/information">
+                            <button type="button">Continuar Pedido</button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import Products from '../components/Products'
+import { Helmet } from 'react-helmet';
+import Products from '../components/Products';
 
 const Home = () => {
     return (
-        <Products />
+        <>
+            <Helmet>
+                <title>Platzi Conf Merch - Productos</title>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@anthony5560"/>
+                <meta name="twitter:creator" content="@anthony5560"/>
+                <meta name="twitter:title" content="Platzi Conf Store"/>
+                <meta name="twitter:description" content="Platzi Conf Store"/>
+                <meta
+                name="twitter:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:title" content="Platzi Conf Store"/>
+                <meta property="og:description" content="Platzi Conf Store"/>
+                <meta
+                property="og:image"
+                content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:url" content="platzistore.xyz" />
+                <meta property="og:site_name" content="Platzi Conf Store" />
+                <meta property="og:locale" content="es_ES" />
+                <meta property="og:type" content="article" />
+                <meta property="fb:app_id" content="92167" />
+            </Helmet>
+            <Products />
+        </>
     );
 }
 
